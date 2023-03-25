7e2484d992a083b343cd6baeb48cc2f4965d3442
Joel Nieto <joelnieto1215@gmail.com>
2023-03-25 12:03:29 -0500

    Config swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,14 +2,23 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Skooltrak API')
+    .setDescription('Skooltrak API build with NestJS')
+    .setVersion('2.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;

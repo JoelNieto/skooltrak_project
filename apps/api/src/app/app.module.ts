52dc88461d24823a60ea8aae69fbdbf20859cfbb
Joel Nieto <joelnieto1215@gmail.com>
2023-03-25 22:38:01 -0500

    Adding env config

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
+import { getEnvPath } from '../common/helper/env.helper';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const envFilePath = getEnvPath(`${process.cwd()}/apps/api/src/common/env`);
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })

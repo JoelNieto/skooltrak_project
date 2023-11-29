60a8c47dd4a1856bc68b9e5af8821d6359fddd76
Joel Nieto <joel@quadraticsoft.com>
2023-11-28 22:56:07 -0500

    Chaging stores folders name

---
+++ libs/store/src/lib/stores/auth/reducer.ts
@@ -0,0 +1,48 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { SchoolUser, User } from '@skooltrak/models';
+import { Session } from '@supabase/supabase-js';
+
+import { AuthActions as actions } from './actions';
+
+export type State = {
+  LOADING: boolean;
+  USER: User | undefined;
+  SESSION: Session | null;
+  PROFILES: SchoolUser[];
+  SCHOOL_ID: string | undefined;
+  ERROR: unknown | undefined;
+};
+
+export const initialState: State = {
+  LOADING: false,
+  PROFILES: [],
+  USER: undefined,
+  SESSION: null,
+  SCHOOL_ID: undefined,
+  ERROR: undefined,
+};
+
+export const authFeature = createFeature({
+  name: 'auth',
+  reducer: createReducer(
+    initialState,
+    on(actions.initState, (state): State => ({ ...state, LOADING: true })),
+    on(
+      actions.setSession,
+      (state, { SESSION }): State => ({ ...state, SESSION }),
+    ),
+    on(
+      actions.setUser,
+      (state, { USER }): State => ({ ...state, USER, LOADING: false }),
+    ),
+    on(
+      actions.setSchoolId,
+      (state, { SCHOOL_ID }): State => ({ ...state, SCHOOL_ID }),
+    ),
+    on(
+      actions.setProfiles,
+      (state, { PROFILES }): State => ({ ...state, PROFILES }),
+    ),
+    on(actions.signOut, (state): State => ({ ...state, ...initialState })),
+  ),
+});
---
+++ libs/store/src/lib/stores/auth/selectors.ts
@@ -0,0 +1,63 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { RoleEnum, School, SchoolUser, User } from '@skooltrak/models';
+
+import { authFeature, State } from './reducer';
+
+export const selectAuthState = createFeatureSelector<State>(authFeature.name);
+
+export const selectSession = createSelector(
+  selectAuthState,
+  (state: State) => state.SESSION,
+);
+
+export const selectLoading = createSelector(
+  selectAuthState,
+  (state: State) => state.LOADING,
+);
+
+export const selectUser = createSelector(
+  selectAuthState,
+  (state: State) => state.USER,
+);
+export const selectUserId = createSelector(
+  selectUser,
+  (state: User | undefined) => state?.id,
+);
+
+export const selectSchoolId = createSelector(
+  selectAuthState,
+  (state) => state.SCHOOL_ID,
+);
+
+export const selectProfiles = createSelector(
+  selectAuthState,
+  (state) => state.PROFILES,
+);
+
+export const selectSchools = createSelector(selectProfiles, (state) => [
+  ...new Set(state.map((x) => x.school)),
+]);
+
+export const selectSchool = createSelector(
+  selectSchools,
+  selectSchoolId,
+  (schools: Partial<School>[], id: string | undefined) =>
+    schools.find((x) => x.id === id),
+);
+
+export const selectRoles = createSelector(
+  selectProfiles,
+  selectSchoolId,
+  (profiles: SchoolUser[], id: string | undefined) =>
+    profiles.filter((x) => x.school_id === id).map((x) => x.role),
+);
+
+export const selectIsAdmin = createSelector(selectRoles, (roles) =>
+  roles.includes(RoleEnum.Administrator),
+);
+export const selectIsTeacher = createSelector(selectRoles, (roles) =>
+  roles.includes(RoleEnum.Teacher),
+);
+export const selectIsStudent = createSelector(selectRoles, (roles) =>
+  roles.includes(RoleEnum.Student),
+);

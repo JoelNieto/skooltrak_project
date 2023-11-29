5b2caaea9ee143f798985426628c6a38c252c0d8
Joel Nieto <joel@quadraticsoft.com>
2023-11-28 22:56:00 -0500

    Chaging stores folders name

--- libs/store/src/index.ts
+++ libs/store/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/services/supabase.service';
 export * from './lib/services/password.validator';
-export * as authState from './lib/states/auth';
-export * as messagingState from './lib/states/messaging';
+export * as authState from './lib/stores/auth';
+export * as messagingState from './lib/stores/messaging';

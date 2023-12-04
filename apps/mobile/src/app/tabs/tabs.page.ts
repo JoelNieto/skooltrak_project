81d3a1b5369aa15ab5a0d608dd00f17b463ef15c
Joel Nieto <joel@quadraticsoft.com>
2023-12-03 22:02:08 -0500

    Adding vite

--- apps/mobile/src/app/tabs/tabs.page.ts
+++ apps/mobile/src/app/tabs/tabs.page.ts
@@ -8,15 +8,15 @@ import { IonicModule } from '@ionic/angular';
   template: `<ion-tabs>
     <ion-tab-bar slot="bottom">
       <ion-tab-button tab="tab1">
-        <ion-icon aria-hidden="true" name="home" />
+        <ion-icon aria-hidden="true" name="home-outline" />
         Home
       </ion-tab-button>
       <ion-tab-button tab="messages">
-        <ion-icon aria-hidden="true" name="chatbubbles" />
+        <ion-icon aria-hidden="true" name="chatbubbles-outline" />
         Messages
       </ion-tab-button>
       <ion-tab-button tab="tab3">
-        <ion-icon aria-hidden="true" name="build" />
+        <ion-icon aria-hidden="true" name="build-outline" />
         Setting
       </ion-tab-button>
     </ion-tab-bar>

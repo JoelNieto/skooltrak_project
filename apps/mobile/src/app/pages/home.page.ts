16c8eb637101e1204bc53bebc8459251fdc460dd
Joel Nieto <joel@quadraticsoft.com>
2023-12-11 15:28:20 -0500

    Signal store for messaging mobile

--- apps/mobile/src/app/pages/home.page.ts
+++ apps/mobile/src/app/pages/home.page.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonicModule, NavController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   standalone: true,
@@ -8,21 +8,10 @@ import { IonicModule, NavController } from '@ionic/angular';
   imports: [IonicModule, RouterLink],
   template: `<ion-header class="ion-no-border">
       <ion-toolbar>
-        <ion-buttons slot="end">
-          <ion-button (click)="goToMessages()">
-            <ion-icon slot="icon-only" name="chatbubbles-outline" size="large"
-          /></ion-button>
-        </ion-buttons>
         <ion-title> Inicio </ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content [fullscreen]="true"> </ion-content> `,
 })
-export class HomePage {
-  private readonly nav = inject(NavController);
-
-  public goToMessages(): void {
-    this.nav.navigateForward('/messages');
-  }
-}
+export class HomePage {}
